5ba5eb9345f9c3a38744698165c8d53600948fd8
Himanshuyadav1 <himanshu.tj@gmail.com>
2026-05-02 17:55:17 +0530

    Created Bday Card

---
+++ script.js
@@ -0,0 +1,13 @@
+var a=prompt("Enter Name");
+ var b=prompt("Enter DOB");
+  	if(a== "Shefali" && b=="02-05-1993")
+  	{
+		alert("Welcome Shefali 😊");
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
